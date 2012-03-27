2683087db102083887989b123825bbf1ee70b7f5
chuacheeseng83@gmail.com <chuacheeseng83@gmail.com@fd1274fd-080f-5963-8033-43ee40fcd1de>
2012-03-27 12:46:50 +0000

    cucumber-runner branch - Changed to fire ScalaTest events instead of println in ScalaTestReporter.

--- branches/cucumber-runner/src/main/scala/org/scalatest/cucumber/ScalaTestReporter.scala
+++ branches/cucumber-runner/src/main/scala/org/scalatest/cucumber/ScalaTestReporter.scala
@@ -13,44 +13,61 @@ import gherkin.formatter.model.Background
 import gherkin.formatter.model.Scenario
 import gherkin.formatter.model.ScenarioOutline
 import gherkin.formatter.model.Examples
+import org.scalatest.Tracker
+import org.scalatest.events.TestStarting
+import org.scalatest.events.MotionToSuppress
+import org.scalatest.events.TestSucceeded
+import org.scalatest.Suite
+import org.scalatest.events.TestPending
+import org.scalatest.events.TestFailed
 
-class ScalaTestReporter(reporter: Reporter, formatter: Formatter, dispatch: org.scalatest.Reporter) extends Reporter with Formatter {
+class ScalaTestReporter(reporter: Reporter, formatter: Formatter, suiteClass: Class[_], tracker: Tracker, dispatch: org.scalatest.Reporter) extends Reporter with Formatter {
 
   private val steps = new ListBuffer[Step]()
   private var currentStep: Step = null
+  private var indentLevel: Int = 0
   
   private def getTestName(step: Step) = step.getKeyword + ": " + step.getName
   
   def result(result: Result) {
     val error = result.getError
+    val testName = getTestName(currentStep)
+    val formatter = Suite.getIndentedText(testName, indentLevel + 1, true)
+    
     if (Result.SKIPPED == result) {
       // Skipped, should map to Ignore
-      println("#####SKIPPED - " + getTestName(currentStep))
+      //println("#####SKIPPED - " + getTestName(currentStep))
+      dispatch(TestPending(tracker.nextOrdinal(), suiteClass.getSimpleName, Some(suiteClass.getName), testName, Some(formatter)))
     }
     else if (Result.UNDEFINED == result) {
       // This happens when step is not defined, should map to canceled?
-      println("####UNDEFINED - " + getTestName(currentStep))
+      //println("####UNDEFINED - " + getTestName(currentStep))
+      dispatch(TestFailed(tracker.nextOrdinal(), "Step is not defined.", suiteClass.getSimpleName, Some(suiteClass.getName), testName, None, Some(result.getDuration), Some(formatter), None))
     }
     else if (error != null) {
       if (error.isInstanceOf[PendingException]) {
         // Pending
-        println("#####PENDING - " + getTestName(currentStep))
+        //println("#####PENDING - " + getTestName(currentStep))
+        dispatch(TestPending(tracker.nextOrdinal(), suiteClass.getSimpleName, Some(suiteClass.getName), testName, Some(formatter)))
       }
       else {
         // Failed
-        println("#####FAILED - " + getTestName(currentStep))
+        //println("#####FAILED - " + getTestName(currentStep))
+        dispatch(TestFailed(tracker.nextOrdinal(), error.getMessage, suiteClass.getSimpleName, Some(suiteClass.getName), testName, Some(error), Some(result.getDuration), Some(formatter), None))
       }
     }
     else {
       // Success
-      println("#####SUCCESS - " + getTestName(currentStep))
+      //println("#####SUCCESS - " + getTestName(currentStep))
+      dispatch(TestSucceeded(tracker.nextOrdinal(), suiteClass.getSimpleName, Some(suiteClass.getName), testName, Some(result.getDuration), Some(formatter), None))
     }
     reporter.result(result)
   }
   
   def `match`(m: Match) {
     currentStep = steps.remove(0)
-    println("#####TEST STARTING - " + getTestName(currentStep))
+    //println("#####TEST STARTING - " + getTestName(currentStep))
+    dispatch(TestStarting(tracker.nextOrdinal(), suiteClass.getSimpleName, Some(suiteClass.getName), getTestName(currentStep), Some(MotionToSuppress), None))
     reporter.`match`(m)
   }
   
