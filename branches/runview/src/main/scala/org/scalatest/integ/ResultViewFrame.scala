0ad3737234598b4214fc890b8b9ec9ea040a53a9
chuacheeseng83@gmail.com <chuacheeseng83@gmail.com@fd1274fd-080f-5963-8033-43ee40fcd1de>
2012-05-11 09:29:57 +0000

    runview branch - Added counter panel in ResultView.

---
+++ branches/runview/src/main/scala/org/scalatest/integ/ResultViewFrame.scala
@@ -0,0 +1,24 @@
+package org.scalatest.integ
+import javax.swing.JFrame
+import javax.swing.WindowConstants
+import java.util.Observer
+import java.util.Observable
+
+class ResultViewFrame extends JFrame with Observer {
+  private val content = getContentPane()
+  private val resultView = new ResultView()
+  
+  setTitle("ScalaTest Run Result")
+  setSize(360, 100)
+  content.add(resultView)
+  
+  setVisible(true)
+  setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
+  
+  def update(o: Observable, event: AnyRef) {
+    event match {
+      case e: Event => resultView.update(o, event)
+      case _ => // Ignore others
+    }
+  }
+}
