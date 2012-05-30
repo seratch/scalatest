4cd6aa20db3d2178e2205827f208491dce9171fb
chuacheeseng83@gmail.com <chuacheeseng83@gmail.com@fd1274fd-080f-5963-8033-43ee40fcd1de>
2012-05-30 04:10:36 +0000

    Created branch trunk-cs2 from branch.

---
+++ branches/trunk-cs2/src/main/scala/org/scalatest/tools/ConcurrentDistributor.scala
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2001-2008 Artima, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.scalatest.tools
+
+import org.scalatest._
+import java.util.concurrent.Executors
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.LinkedBlockingQueue
+import java.util.concurrent.Future
+
+/**
+ * This Distributor can be used by multiple threads.
+ *
+ * @author Bill Venners
+ */
+private[scalatest] class ConcurrentDistributor(reporter: Reporter, stopper: Stopper, configMap: Map[String, Any], execSvc: ExecutorService) extends Distributor {
+
+  private val futureQueue = new LinkedBlockingQueue[Future[T] forSome { type T }]
+
+  // TODO: Chee Seng, we can't not use this method. This the public API of Distributor. Must
+  // find a different way. Can you pass Filter to the constructor of ConcurrentDistributor? If not,
+  // we could consider adding Filter as a 3rd argument to apply in Distributor. I don't understand
+  // what you were trying to accomplish enough to know. Let's talk about it.
+  def apply(suite: Suite, tracker: Tracker) {
+    throw new UnsupportedOperationException("ConcurrentDistributor does not support this operation, please use apply(suite: Suite, tracker: Tracker, filter: Filter) instead.")
+  }
+ 
+  def apply(suite: Suite, tracker: Tracker, filter: Filter) {
+    val suiteRunner = new SuiteRunner(suite, reporter, stopper, filter, configMap, Some(this), tracker)
+    val future: Future[_] = execSvc.submit(suiteRunner)
+    futureQueue.put(future)
+  }
+
+  def poll() = None
+
+  def waitUntilDone() {
+    while (futureQueue.peek != null)
+      futureQueue.poll().get()
+  }
+}
+
