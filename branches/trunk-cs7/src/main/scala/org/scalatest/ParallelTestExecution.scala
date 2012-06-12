bd3183c60d7a99905120c45c6ec3737c2c0bd87b
chuacheeseng83@gmail.com <chuacheeseng83@gmail.com@fd1274fd-080f-5963-8033-43ee40fcd1de>
2012-06-12 09:56:55 +0000

    Created trunk-cs7 branch for SuiteSortingReporter development.

---
+++ branches/trunk-cs7/src/main/scala/org/scalatest/ParallelTestExecution.scala
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2001-2009 Artima, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.scalatest    
+
+import tools.DistributedTestRunnerSuite
+import OneInstancePerTest.RunTestInNewInstance
+import org.scalatest.tools.TestSortingReporter
+import org.scalatest.time.Span
+import org.scalatest.time.Seconds
+import org.scalatest.tools.Runner
+
+/**
+ * Trait that causes that the tests of any suite it is mixed into to be run in parallel if
+ * a <code>Distributor</code> is passed to <code>runTests</code>.
+ *
+ * <p>
+ * ScalaTest's normal approach for running suites of tests in parallel is to run different suites in parallel,
+ * but the tests of any one suite sequentially. This approach should provide sufficient distribution of the work load
+ * in most cases, but some suites may encapsulate multiple long-running tests. Such suites may dominate the execution
+ * time of the run. If so, mixing in this trait into just those suites will allow their long-running tests to run in parallel with each
+ * other, thereby helping to reduce the total time required to run an entire run.
+ * </p>
+ *
+ * <p>
+ * Because this trait extends <code>OneInstancePerTest</code>,
+ * each test will be run its own instance of the suite's class. This trait overrides the 
+ * <code>runTests</code> method. If no <code>Distributor</code> is passed to <code>runTests</code>, 
+ * this trait's implementation simply invokes its supertrait <code>OneInstancePerTest</code>'s implementation
+ * of <code>runTests</code>, which will run each test in its own instance sequentially. If a <code>Distributor</code>
+ * is passed, however, this traits' implementation of <code>runTests</code> will, for each test, wrap a new instance of the
+ * suite in a special <em>wrapper suite</em> that will invoke just that one test, and passes the wrapper suites to the <code>Distributor</code>.
+ * The thread or entity that takes a wrapper suite from the <code>Distributor</code> will invoke <code>run</code>
+ * on the wrapper suite, which will run just one test. In this way, different tests of a suite that mixes in
+ * <code>ParallelTestExecution</code> will run in parallel.
+ * </p>
+ *
+ * @author Bill Venners
+ */
+trait ParallelTestExecution extends OneInstancePerTest { this: Suite =>
+  
+  @volatile private var sortingReporter: Option[TestSortingReporter] = None
+  
+  protected abstract override def runTests(testName: Option[String], args: RunArgs) {
+    if (args.configMap.contains(RunTestInNewInstance)) {
+      super.runTests(testName, args)
+    }
+    else {
+      sortingReporter = Some(new TestSortingReporter(args.reporter, timeout))
+      val newArgs = args.copy(reporter = sortingReporter.get)
+      super.runTests(testName, newArgs)
+    }
+  }
+
+  protected abstract override def runTest(testName: String, args: RunArgs) {
+
+    if (args.configMap.contains(RunTestInNewInstance)) {
+      val oneInstance = newInstance
+      args.distributor match {
+        case None =>
+          oneInstance.run(Some(testName), args)
+        case Some(distribute) =>
+          sortingReporter match {
+            case Some(sortingReporter) => 
+              sortingReporter.waitForTestCompleted(testName)
+            case None => 
+          }
+          distribute(new DistributedTestRunnerSuite(oneInstance, testName, args), args.tracker.nextTracker)
+      }
+    }
+    else
+      super.runTest(testName, args)
+  }
+
+  // Narrow the type
+  override def newInstance: Suite with ParallelTestExecution = {
+    val instance = getClass.newInstance.asInstanceOf[Suite with ParallelTestExecution]
+    instance
+  }
+  
+  protected def timeout: Span = Runner.testSortingReporterTimeout
+}
---
+++ branches/trunk-cs7/src/main/scala/org/scalatest/tools/TestSortingReporter.scala
@@ -0,0 +1,144 @@
+package org.scalatest.tools
+
+import org.scalatest._
+import org.scalatest.events._
+import DispatchReporter.propagateDispose
+import scala.collection.mutable.ListBuffer
+import org.scalatest.time.Span
+import java.util.Timer
+import java.util.TimerTask
+
+private[scalatest] class TestSortingReporter(dispatch: Reporter, timeout: Span) extends ResourcefulReporter {
+
+  case class Slot(testName: String, startEvent: Option[Event], completedEvent: Option[Event], ready: Boolean)
+  
+  private val waitingBuffer = new ListBuffer[Slot]()
+  private val slotMap = new collection.mutable.HashMap[String, Slot]()  // testName -> Slot
+  
+  class TimeoutTask(val event: Event) extends TimerTask {
+    override def run() {
+      timeout()
+    }
+  }
+  
+  private val timer = new Timer()
+  private var timeoutTask: Option[TimeoutTask] = None
+  
+  def waitForTestCompleted(testName: String) {
+    synchronized {
+      slotMap.put(testName, Slot(testName, None, None, false))
+    }
+  }
+  
+  override def apply(event: Event) {
+    synchronized {
+      event match {
+        case testStarting: TestStarting => 
+          slotMap.get(testStarting.testName) match {
+            case Some(slot) => 
+              val newSlot = slot.copy(startEvent = Some(testStarting))
+              waitingBuffer += newSlot
+              slotMap.put(testStarting.testName, newSlot)
+            case None => 
+              dispatch(testStarting)
+          }
+        case testIgnored: TestIgnored => 
+          slotMap.get(testIgnored.testName) match {
+            case Some(slot) => 
+              val newSlot = slot.copy(startEvent = Some(testIgnored), ready = true)
+              waitingBuffer += newSlot
+              slotMap.put(testIgnored.testName, newSlot)
+            case None => 
+              dispatch(testIgnored)
+          }
+        case testSucceeded: TestSucceeded => 
+          handleTestCompleted(testSucceeded, testSucceeded.testName)
+        case testFailed: TestFailed => 
+          handleTestCompleted(testFailed, testFailed.testName)
+        case testPending: TestPending => 
+          handleTestCompleted(testPending, testPending.testName)
+        case testCanceled: TestCanceled => 
+          handleTestCompleted(testCanceled, testCanceled.testName)
+        case _ => 
+          dispatch(event)
+      }
+      fireReadyEvents()
+    }
+  }
+  
+  private def handleTestCompleted(event: Event, testName: String) {
+    slotMap.get(testName) match {
+      case Some(slot) => 
+        val newSlot = slot.copy(completedEvent = Some(event), ready = true)
+        waitingBuffer.update(waitingBuffer.indexOf(slot), newSlot)
+        slotMap.put(testName, newSlot)
+      case None => 
+        dispatch(event)
+    }
+  }
+  
+  private def fireReadyEvents() {
+    val (ready, pending) = waitingBuffer.span(slot => slot.ready)
+    ready.foreach { slot => 
+      dispatch(slot.startEvent.get)
+      slot.completedEvent match {
+        case Some(completedEvent) => 
+          dispatch(completedEvent)
+          completedEvent match {
+            case testSucceeded: TestSucceeded => 
+              testSucceeded.recordedEvents.foreach(dispatch(_))
+            case testFailed: TestFailed => 
+              testFailed.recordedEvents.foreach(dispatch(_))
+            case testPending: TestPending => 
+              testPending.recordedEvents.foreach(dispatch(_))
+            case testCanceled: TestCanceled => 
+              testCanceled.recordedEvents.foreach(dispatch(_))
+          }
+        case None =>
+      }
+    }
+    waitingBuffer.clear()
+    waitingBuffer ++= pending
+    if (waitingBuffer.size > 0) 
+      scheduleTimeoutTask()
+    else {
+      timeoutTask match {
+        case Some(task) => 
+          task.cancel()
+          timeoutTask = None
+        case None =>
+      }
+    }
+  }
+  
+  private def scheduleTimeoutTask() {
+    val head = waitingBuffer.head
+    timeoutTask match {
+        case Some(task) => 
+          if (head.startEvent.get != task.event) {
+            task.cancel()
+            timeoutTask = Some(new TimeoutTask(head.startEvent.get))
+            timer.schedule(timeoutTask.get, timeout.millisPart)
+          }
+        case None => 
+          timeoutTask = Some(new TimeoutTask(head.startEvent.get))
+          timer.schedule(timeoutTask.get, timeout.millisPart)
+      }
+  }
+  
+  private def timeout() {
+    synchronized {
+      if (waitingBuffer.size > 0) {
+        val head = waitingBuffer.head
+        if (timeoutTask.get.event == head.startEvent.get) {
+          val newSlot = head.copy(ready = true)
+          waitingBuffer.update(0, newSlot)
+          slotMap.remove(newSlot.testName)
+        }
+        fireReadyEvents()
+      }
+    }
+  }
+  
+  override def dispose() = propagateDispose(dispatch)
+}
