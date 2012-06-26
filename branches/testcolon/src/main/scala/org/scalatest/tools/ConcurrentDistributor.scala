39562f1aa6fa786faf542de309fb3956fd05fd17
chuacheeseng83@gmail.com <chuacheeseng83@gmail.com@fd1274fd-080f-5963-8033-43ee40fcd1de>
2012-06-26 10:39:45 +0000

    Created testcolon branch from trunk to add feature to drop leading "test: " of formattedText for decodedTestName that starts with "test: ".

---
+++ branches/testcolon/src/main/scala/org/scalatest/tools/ConcurrentDistributor.scala
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2001-2008 Artima, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.scalatest.tools
+
+import org.scalatest._
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.LinkedBlockingQueue
+import java.util.concurrent.Future
+
+/**
+ * This Distributor can be used by multiple threads.
+ *
+ * @author Bill Venners
+ */
+private[scalatest] class ConcurrentDistributor(args: RunArgs, execSvc: ExecutorService) extends Distributor {
+
+  private val futureQueue = new LinkedBlockingQueue[Future[T] forSome { type T }]
+
+  def apply(suite: Suite, tracker: Tracker) {
+    apply(suite, args.copy(tracker = tracker))
+  }
+ 
+  def apply(suite: Suite, args: RunArgs) {
+    if (suite == null)
+      throw new NullPointerException("suite is null")
+    if (args == null)
+      throw new NullPointerException("args is null")
+    val suiteRunner = new SuiteRunner(suite, args)
+    val future: Future[_] = execSvc.submit(suiteRunner)
+    futureQueue.put(future)
+  }
+
+  def poll() = None
+
+  def waitUntilDone() {
+    while (futureQueue.peek != null)
+      futureQueue.poll().get()
+  }
+}
+
+private[scalatest] class DistributorWrapper(distributor: Distributor, testSortingReporter: TestSortingReporter) extends Distributor {
+
+  def apply(suite: Suite, tracker: Tracker) {
+    waitForTestCompleted(suite)
+    distributor.apply(suite, tracker)
+  }
+
+  def apply(suite: Suite, args: RunArgs) {
+    waitForTestCompleted(suite)
+    distributor.apply(suite, args)
+  }
+
+  private def waitForTestCompleted(suite: Suite) {
+    suite match {
+      case dtrs: DistributedTestRunnerSuite =>
+        testSortingReporter.distributingTest(dtrs.testName)
+      case _ =>
+    }
+  }
+}
+
---
+++ branches/testcolon/src/test/scala/org/scalatest/SharedHelpers.scala
@@ -0,0 +1,323 @@
+/*
+ * Copyright 2001-2008 Artima, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.scalatest
+
+import org.scalatest.events._
+
+trait SharedHelpers extends Assertions {
+
+  object SilentReporter extends Reporter {
+    def apply(event: Event) = ()  
+  }
+
+  class TestDurationReporter extends Reporter {
+    var testSucceededWasFiredAndHadADuration = false
+    var testFailedWasFiredAndHadADuration = false
+    override def apply(event: Event) {
+      event match {
+        case event: TestSucceeded => testSucceededWasFiredAndHadADuration = event.duration.isDefined
+        case event: TestFailed => testFailedWasFiredAndHadADuration = event.duration.isDefined
+        case _ =>
+      }
+    }
+  }
+
+  class SuiteDurationReporter extends Reporter {
+    var suiteCompletedWasFiredAndHadADuration = false
+    var suiteAbortedWasFiredAndHadADuration = false
+    override def apply(event: Event) {
+      event match {
+        case event: SuiteCompleted => suiteCompletedWasFiredAndHadADuration = event.duration.isDefined
+        case event: SuiteAborted => suiteAbortedWasFiredAndHadADuration = event.duration.isDefined
+        case _ =>
+      }
+    }
+  }
+
+  class PendingReporter extends Reporter {
+    var testPendingWasFired = false
+    override def apply(event: Event) {
+      event match {
+        case _: TestPending => testPendingWasFired = true
+        case _ =>
+      }
+    }
+  }
+
+  class EventRecordingReporter extends Reporter {
+    private var eventList: List[Event] = List()
+    def eventsReceived = eventList.reverse
+    def testSucceededEventsReceived: List[TestSucceeded] = {
+      eventsReceived filter {
+        case event: TestSucceeded => true
+        case _ => false
+      } map {
+        case event: TestSucceeded => event
+        case _ => throw new RuntimeException("should never happen")
+      }
+    }
+    def testStartingEventsReceived: List[TestStarting] = {
+      eventsReceived filter {
+        case event: TestStarting => true
+        case _ => false
+      } map {
+        case event: TestStarting => event
+        case _ => throw new RuntimeException("should never happen")
+      }
+    }
+    // Why doesn't this work:
+    // for (event: TestSucceeded <- eventsReceived) yield event
+    def infoProvidedEventsReceived: List[InfoProvided] = {
+      eventsReceived filter {
+        case event: InfoProvided => true
+        case _ => false
+      } map {
+        case event: InfoProvided => event
+        case _ => throw new RuntimeException("should never happen")
+      }
+    }
+    def markupProvidedEventsReceived: List[MarkupProvided] = {
+      eventsReceived filter {
+        case event: MarkupProvided => true
+        case _ => false
+      } map {
+        case event: MarkupProvided => event
+        case _ => throw new RuntimeException("should never happen")
+      }
+    }
+    def scopeOpenedEventsReceived: List[ScopeOpened] = {
+      eventsReceived filter {
+        case event: ScopeOpened => true
+        case _ => false
+      } map {
+        case event: ScopeOpened => event
+        case _ => throw new RuntimeException("should never happen")
+      }
+    }
+    def scopeClosedEventsReceived: List[ScopeClosed] = {
+      eventsReceived filter {
+        case event: ScopeClosed => true
+        case _ => false
+      } map {
+        case event: ScopeClosed => event
+        case _ => throw new RuntimeException("should never happen")
+      }
+    }
+    def testPendingEventsReceived: List[TestPending] = {
+      eventsReceived filter {
+        case event: TestPending => true
+        case _ => false
+      } map {
+        case event: TestPending => event
+        case _ => throw new RuntimeException("should never happen")
+      }
+    }
+    def testCanceledEventsReceived: List[TestCanceled] = {
+      eventsReceived filter {
+        case event: TestCanceled => true
+        case _ => false
+      } map {
+        case event: TestCanceled => event
+        case _ => throw new RuntimeException("should never happen")
+      }
+    }
+    def testFailedEventsReceived: List[TestFailed] = {
+      eventsReceived filter {
+        case event: TestFailed => true
+        case _ => false
+      } map {
+        case event: TestFailed => event
+        case _ => throw new RuntimeException("should never happen")
+      }
+    }
+    def testIgnoredEventsReceived: List[TestIgnored] = {
+      eventsReceived filter {
+        case event: TestIgnored => true
+        case _ => false
+      } map {
+        case event: TestIgnored => event
+        case _ => throw new RuntimeException("should never happen")
+      }
+    }
+    def suiteStartingEventsReceived: List[SuiteStarting] = {
+      eventsReceived filter {
+        case event: SuiteStarting => true
+        case _ => false
+      } map {
+        case event: SuiteStarting => event
+        case _ => throw new RuntimeException("should never happen")
+      }
+    }
+    def apply(event: Event) {
+      eventList ::= event
+    }
+  }
+  
+  def getIndexesForTestInformerEventOrderTests(suite: Suite, testName: String, infoMsg: String): (Int, Int) = {
+    val myRep = new EventRecordingReporter
+    suite.run(None, RunArgs(myRep, new Stopper {}, Filter(), Map(), None, new Tracker, Set.empty))
+
+    val indexedList = myRep.eventsReceived.zipWithIndex
+
+    val testStartingOption = indexedList.find(_._1.isInstanceOf[TestStarting])
+    val testSucceededOption = indexedList.find(_._1.isInstanceOf[TestSucceeded])
+    
+    assert(testStartingOption.isDefined, "TestStarting for Suite='" + suite.suiteId + "', testName='" + testName + "' not defined.")
+    assert(testSucceededOption.isDefined, "TestSucceeded for Suite='" + suite.suiteId + "', testName='" + testName + "' not defined.")
+    
+    val testStartingIndex = testStartingOption.get._2
+    val testSucceededIndex = testSucceededOption.get._2
+    
+    val testStarting = testStartingOption.get._1.asInstanceOf[TestStarting]
+    val testSucceeded = testSucceededOption.get._1.asInstanceOf[TestSucceeded]
+    
+    val recordedEvents = testSucceeded.recordedEvents
+    
+    val infoProvidedOption = recordedEvents.find {
+      case event: InfoProvided => event.message == infoMsg
+      case _ => false
+    }
+    assert(infoProvidedOption.isDefined, "InfoProvided for Suite='" + suite.suiteId + "', testName='" + testName + "' not defined.")
+    
+    (testStartingIndex, testSucceededIndex)
+  }
+
+  def getIndexesForInformerEventOrderTests(suite: Suite, testName: String, infoMsg: String): (Int, Int, Int) = {
+
+    val myRep = new EventRecordingReporter
+    suite.run(None, RunArgs(myRep, new Stopper {}, Filter(), Map(), None, new Tracker, Set.empty))
+
+    val indexedList = myRep.eventsReceived.zipWithIndex
+
+    val testStartingOption = indexedList.find(_._1.isInstanceOf[TestStarting])
+    val infoProvidedOption = indexedList.find {
+      case (event: InfoProvided, index) => event.message == infoMsg
+      case _ => false
+    }
+    val testSucceededOption = indexedList.find(_._1.isInstanceOf[TestSucceeded])
+
+    assert(testStartingOption.isDefined, "TestStarting for Suite='" + suite.suiteId + "', testName='" + testName + "' not defined.")
+    assert(infoProvidedOption.isDefined, "InfoProvided for Suite='" + suite.suiteId + "', testName='" + testName + "' not defined.")
+    assert(testSucceededOption.isDefined, "TestSucceeded for Suite='" + suite.suiteId + "', testName='" + testName + "' not defined.")
+
+    val testStartingIndex = testStartingOption.get._2
+    val infoProvidedIndex = infoProvidedOption.get._2
+    val testSucceededIndex = testSucceededOption.get._2
+
+    val testStarting = testStartingOption.get._1.asInstanceOf[TestStarting]
+    val infoProvided = infoProvidedOption.get._1.asInstanceOf[InfoProvided]
+    val testSucceeded = testSucceededOption.get._1.asInstanceOf[TestSucceeded]
+
+    assert(testStarting.testName === testName, "TestStarting.testName expected to be '" + testName + "', but got '" + testStarting.testName + "'.")
+    assert(infoProvided.message === infoMsg, "InfoProvide.message expected to be '" + infoMsg + "', but got '" + infoProvided.message + "'.")
+    assert(testSucceeded.testName === testName, "TestSucceeded.testName expected to be '" + testName + "', but got '" + testSucceeded.testName + "'.")
+
+    (infoProvidedIndex, testStartingIndex, testSucceededIndex)
+  }
+
+  def getIndentedTextFromInfoProvided(suite: Suite): IndentedText = {
+
+    val myRep = new EventRecordingReporter
+    suite.run(None, RunArgs(myRep, new Stopper {}, Filter(), Map(), None, new Tracker, Set.empty))
+
+    val infoProvidedOption = myRep.eventsReceived.find(_.isInstanceOf[InfoProvided])
+
+    infoProvidedOption match {
+      case Some(infoProvided: InfoProvided) =>
+        infoProvided.formatter match {
+          case Some(indentedText: IndentedText) => indentedText
+          case _ => fail("An InfoProvided was received that didn't include an IndentedText formatter: " + infoProvided.formatter)
+        }
+      case _ => fail("No InfoProvided was received by the Reporter during the run.")
+    }
+  }
+  
+  def getIndentedTextFromTestInfoProvided(suite: Suite): IndentedText = {
+    val myRep = new EventRecordingReporter
+    suite.run(None, RunArgs(myRep, new Stopper {}, Filter(), Map(), None, new Tracker, Set.empty))
+    val recordedEvents: Seq[Event] = myRep.eventsReceived.find { e => 
+      e match {
+        case testSucceeded: TestSucceeded => 
+          true
+        case testFailed: TestFailed => 
+          true
+        case testPending: TestPending => 
+          true
+        case testCanceled: TestCanceled =>
+          true
+        case _ => 
+          false
+      }
+    } match {
+      case Some(testCompleted) =>
+        testCompleted match {
+          case testSucceeded: TestSucceeded => 
+            testSucceeded.recordedEvents
+          case testFailed: TestFailed => 
+            testFailed.recordedEvents
+          case testPending: TestPending => 
+            testPending.recordedEvents
+          case testCanceled: TestCanceled =>
+            testCanceled.recordedEvents
+        }
+      case None => 
+        fail("Test completed event is expected but not found.")
+    }
+    assert(recordedEvents.size === 1)
+    recordedEvents(0) match {
+      case ip: InfoProvided => 
+        ip.formatter match {
+          case Some(indentedText: IndentedText) => indentedText
+          case _ => fail("An InfoProvided was received that didn't include an IndentedText formatter: " + ip.formatter)
+        }
+      case _ => fail("No InfoProvided was received by the Reporter during the run.")
+    }
+  }
+
+  def ensureTestFailedEventReceived(suite: Suite, testName: String) {
+    val reporter = new EventRecordingReporter
+    suite.run(None, RunArgs(reporter, new Stopper {}, Filter(), Map(), None, new Tracker, Set.empty))
+    val testFailedEvent = reporter.eventsReceived.find(_.isInstanceOf[TestFailed])
+    assert(testFailedEvent.isDefined)
+    assert(testFailedEvent.get.asInstanceOf[TestFailed].testName === testName)
+  }
+  
+  def thisLineNumber = {
+    val st = Thread.currentThread.getStackTrace
+
+    if (!st(2).getMethodName.contains("thisLineNumber"))
+      st(2).getLineNumber
+    else
+      st(3).getLineNumber
+  }
+
+  class TestIgnoredTrackingReporter extends Reporter {
+    var testIgnoredReceived = false
+    var lastEvent: Option[TestIgnored] = None
+    def apply(event: Event) {
+      event match {
+        case event: TestIgnored =>
+          testIgnoredReceived = true
+          lastEvent = Some(event)
+        case _ =>
+      }
+    }
+  }
+}
+
+// Selfless trait pattern
+object SharedHelpers extends SharedHelpers
+
