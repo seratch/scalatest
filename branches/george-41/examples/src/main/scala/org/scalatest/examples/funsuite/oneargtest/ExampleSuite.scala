f654ca818e979207460e658918fe674b0dfd7d27
george.berger <george.berger@fd1274fd-080f-5963-8033-43ee40fcd1de>
2012-10-01 16:28:00 +0000

    created new branch branches/george-41 from trunk

---
+++ branches/george-41/examples/src/main/scala/org/scalatest/examples/funsuite/oneargtest/ExampleSuite.scala
@@ -0,0 +1,36 @@
+package org.scalatest.examples.funsuite.oneargtest
+
+import org.scalatest.fixture
+import java.io._
+
+class ExampleSuite extends fixture.FunSuite {
+
+  case class F(file: File, writer: FileWriter)
+  type FixtureParam = F
+
+  def withFixture(test: OneArgTest) {
+
+    // create the fixture
+    val file = File.createTempFile("hello", "world")
+    val writer = new FileWriter(file)
+    val theFixture = F(file, writer)
+
+    try {
+      writer.write("ScalaTest is ") // set up the fixture
+      withFixture(test.toNoArgTest(theFixture)) // "loan" the fixture to the test
+    }
+    finally writer.close() // clean up the fixture
+  }
+
+  test("Testing should be easy") { f =>
+    f.writer.write("easy!")
+    f.writer.flush()
+    assert(f.file.length === 18)
+  }
+
+  test("Testing should be fun") { f =>
+    f.writer.write("fun!")
+    f.writer.flush()
+    assert(f.file.length === 17)
+  }
+}
