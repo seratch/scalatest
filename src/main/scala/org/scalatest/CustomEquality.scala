e21c3fd43fbba94dd1892e8576ce9f1f3299dc47
billvenners <billvenners@gmail.com>
2012-10-31 00:43:55 +0000

    init ver.

---
+++ src/main/scala/org/scalatest/CustomEquality.scala
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2001-20012 Artima, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.scalatest
+
+trait CustomEquality extends NonImplicitAssertions with LowPriorityEqualityImplicits {
+
+  abstract class EqualityResult[A, B]
+  case class UnexpectedEqualityResult[A, B](msg: String) extends EqualityResult[A, B]
+  case class ExpectedEqualityResult[A, B] extends EqualityResult[A, B]
+  
+  class Equalizer[L](left: L) {
+    def ===[R](right: R)(implicit equality: Equality[L, R]): EqualityResult[L, R] = {
+      if (equality.areEqual(left, right)) ExpectedEqualityResult()
+      else UnexpectedEqualityResult(left + " did not equal " + right)
+    }
+    def !==[R](right: R)(implicit equality: Equality[L, R]): EqualityResult[L, R] = {
+      if (!equality.areEqual(left, right)) ExpectedEqualityResult()
+      else UnexpectedEqualityResult(left + " did not equal " + right)
+    }
+  }
+  
+  class SeqEqualizer[L](left: Seq[L]) {
+    println("Created a SeqEqualizer")
+    def ===[R](right: Seq[R])(implicit equality: Equality[Seq[L], Seq[R]]): EqualityResult[Seq[L], Seq[R]] = {
+      if (equality.areEqual(left, right)) ExpectedEqualityResult()
+      else UnexpectedEqualityResult(left + " did not equal " + right)
+    }
+  }
+  
+  class MapEqualizer[LK, LV](left: Map[LK, LV]) {
+    println("Created a MapEqualizer")
+    def ===[RK, RV](right: Map[RK, RV])(implicit equality: Equality[Map[LK, LV], Map[RK, RV]]): EqualityResult[Map[LK, LV], Map[RK, RV]] = {
+      if (equality.areEqual(left, right)) ExpectedEqualityResult()
+      else UnexpectedEqualityResult(left + " did not equal " + right)
+    }
+  }
+  
+  class SetEqualizer[L](left: Set[L]) {
+    println("Created a SetEqualizer")
+    def ===[R](right: Set[R])(implicit equality: Equality[Set[L], Set[R]]): EqualityResult[Set[L], Set[R]] = {
+      if (equality.areEqual(left, right)) ExpectedEqualityResult()
+      else UnexpectedEqualityResult(left + " did not equal " + right)
+    }
+  }
+  
+  def assert[L, R](eqRes: EqualityResult[L, R]) {
+    eqRes match {
+      case UnexpectedEqualityResult(msg) => throw new AssertionError(msg)
+      case _ =>
+    }
+  }
+  
+  implicit def convertToEqualizer[T](o: T) = new Equalizer(o)
+  implicit def convertToSeqEqualizer[T](o: Seq[T]) = new SeqEqualizer[T](o)
+  implicit def convertToMapEqualizer[K, V](o: Map[K, V]) = new MapEqualizer[K, V](o)
+  implicit def convertToSetEqualizer[T](o: Set[T]) = new SetEqualizer[T](o)
+  
+  implicit def equalityTwo[A, B](implicit ev: B <:< A): Equality[A, B] =
+    new Equality[A, B] {
+      def areEqual(a: A, b: B): Boolean =
+        a == b
+    }
+}
+
+object CustomEquality extends CustomEquality
+
---
+++ src/test/scala/org/scalatest/CustomEqualitySpec.scala
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2001-2008 Artima, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.scalatest
+
+class CustomEqualitySpec extends Spec with CustomEquality {
+
+  import scala.collection.immutable._
+
+  object `the custom equality === operator` {
+
+    def `should compare subclasses with superclases on either side as well as the same type on either side` {
+
+      class Fruit { override def equals(o: Any) = o.isInstanceOf[Fruit] }
+      class Apple extends Fruit
+
+      assert(new Fruit === new Apple)
+      assert(new Apple === new Apple)
+      assert(new Apple === new Fruit)
+
+      assert(1 === 1)
+    }
+
+    def `should compare two Seqs for equality` {
+
+      assert(Vector(1, 2, 3) === IndexedSeq(1, 2, 3)) // superclass on right
+      assert(IndexedSeq(1, 2, 3) === Vector(1, 2, 3)) // superclass on left
+      assert(Vector(1, 2, 3) === List(1, 2, 3)) // no inheritance relationship
+      assert(List(1, 2, 3) === Vector(1, 2, 3)) // converse of above
+      assert(List(1, 2, 3) === List(1, 2, 3)) // same type on both sides
+    }
+
+    def `should compare two Sets for equality` {
+
+      assert(HashSet(1, 2, 3) === Set(1, 2, 3)) // superclass on right
+      assert(Set(1, 2, 3) === HashSet(1, 2, 3)) // superclass on left
+      assert(BitSet(1, 2, 3) === HashSet(1, 2, 3)) // no inheritance relationship
+      assert(HashSet(1, 2, 3) === BitSet(1, 2, 3)) // converse of above
+      assert(Set(1, 2, 3) === Set(1, 2, 3)) // same type on both sides
+    }
+
+    def `should compare two Maps for equality` {
+
+      assert(HashMap(1 -> 1, 2 -> 2, 3 -> 3) === Map(1 -> 1, 2 -> 2, 3 -> 3)) // supertype on right
+      assert(Map(1 -> 1, 2 -> 2, 3 -> 3) === HashMap(1 -> 1, 2 -> 2, 3 -> 3)) // supertype on left
+      assert(TreeMap(1 -> 1, 2 -> 2, 3 -> 3) === HashMap(1 -> 1, 2 -> 2, 3 -> 3)) // no inheritance relationship
+      assert(HashMap(1 -> 1, 2 -> 2, 3 -> 3) === TreeMap(1 -> 1, 2 -> 2, 3 -> 3)) // converse of above
+      assert(Map(1 -> 1, 2 -> 2, 3 -> 3) === Map(1 -> 1, 2 -> 2, 3 -> 3)) // same type on both sides
+    }
+
+    def `should enable custom equality via a user-defined type class` {
+
+      case class User(id: Long, name: String)
+
+      val kat1 = User(1, "Kat")
+      val joe2 = User(2, "Joe")
+      val joe3 = User(3, "Joe")
+
+      assert(kat1 === kat1)
+      assert(joe2 === joe2)
+      assert(joe3 === joe3)
+      assert(kat1 !== joe2)
+      assert(joe2 !== kat1)
+      assert(joe2 !== joe3)
+      assert(joe3 !== joe2)
+     
+      implicit val userEquality =
+        new Equality[User, User] {
+          def areEqual(a: User, b: User): Boolean = a.name == b.name // I.e., define equality without considering id
+        }
+
+      assert(kat1 === kat1)
+      assert(joe2 === joe2)
+      assert(joe3 === joe3)
+      assert(kat1 !== joe2)
+      assert(joe2 !== kat1)
+      assert(joe2 === joe3) // These two should be different from above, because of the type class
+      assert(joe3 === joe2)
+    }
+  }
+/*
+  These don't compile.
+  assert("1" === 1)
+  assert(List(1, 2, 3) === Map(1 -> 1, 2 -> 2, 3 -> 3))
+  assert(Map(1 -> 1, 2 -> 2, 3 -> 3) === List(1, 2, 3))
+  assert(Set(1, 2, 3) === List(1, 2, 3))
+  assert(List(1, 2, 3) === Set(1, 2, 3))
+*/
+}
+  
