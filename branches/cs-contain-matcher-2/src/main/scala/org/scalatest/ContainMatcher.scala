5990e7a77688b7f6aa0e0c0ac9452feea2cde5bb
chuacheeseng83@gmail.com <chuacheeseng83@gmail.com@fd1274fd-080f-5963-8033-43ee40fcd1de>
2013-01-16 07:54:08 +0000

    Changed ContainMatcher to extend Matcher.

--- branches/cs-contain-matcher-2/src/main/scala/org/scalatest/ContainMatcher.scala
+++ branches/cs-contain-matcher-2/src/main/scala/org/scalatest/ContainMatcher.scala
@@ -2,8 +2,9 @@ package org.scalatest
 
 import collection.GenTraversable
 import matchers.MatchResult
+import matchers.Matcher
 
-trait ContainMatcher[T] {
+trait ContainMatcher[T] extends Matcher[GenTraversable[T]] {
   
   def apply(left: GenTraversable[T]): MatchResult
   
