4fc477e9e4eebbdc1356cb0553a0d090cf0325a0
chuacheeseng83@gmail.com <chuacheeseng83@gmail.com@fd1274fd-080f-5963-8033-43ee40fcd1de>
2013-01-17 10:24:53 +0000

    cs-contain-matcher-2 branch - Added 'not' support to 'and' and 'or' for contain matcher.

--- branches/cs-contain-matcher-2/src/test/scala/org/scalatest/ContainMatcherAndOrSpec.scala
+++ branches/cs-contain-matcher-2/src/test/scala/org/scalatest/ContainMatcherAndOrSpec.scala
@@ -28,11 +28,96 @@ class ContainMatcherAndOrSpec extends Spec with Matchers with SharedHelpers {
         left should (contain noneOf (7, 8, 9) and contain theSameElementsAs (right1))
       }
       
-      def `should failed with correctly stack depth and message when one of contain failed` {
+      def `should failed with correctly stack depth and message when first contain failed but second contain passed` {
+        val e = intercept[exceptions.TestFailedException] {
+          List(1, 2, 3) should (contain theSameIteratedElementsAs List(3, 2, 1) and contain theSameElementsAs List(3, 2, 1)) 
+        }
+        e.message should be (Some("List(1, 2, 3) did not contain the same iterated elements as List(3, 2, 1)"))
+        e.failedCodeFileName should be (Some("ContainMatcherAndOrSpec.scala"))
+        e.failedCodeLineNumber should be (Some(thisLineNumber - 4))
+      }
+      
+      def `should failed with correctly stack depth and message when first contain passed but second contain failed` {
         val e = intercept[exceptions.TestFailedException] {
           List(1, 2, 3) should (contain theSameElementsAs List(3, 2, 1) and contain theSameIteratedElementsAs List(3, 2, 1)) 
         }
         e.message should be (Some("List(1, 2, 3) contained the same elements as List(3, 2, 1), but List(1, 2, 3) did not contain the same iterated elements as List(3, 2, 1)"))
+        e.failedCodeFileName should be (Some("ContainMatcherAndOrSpec.scala"))
+        e.failedCodeLineNumber should be (Some(thisLineNumber - 4))
+      }
+      
+      def `should pass when not contain and contain passes` {
+        
+        val left = List(1, 2, 3)
+        val right1 = List(8, 1, 2)
+        val right2 = List(1, 2, 3)
+        
+        left should (not contain theSameElementsAs (right1) and contain theSameElementsAs (right2)) 
+        left should (not contain theSameIteratedElementsAs (right1) and contain theSameElementsAs (right2))
+        left should (not contain allOf (8, 2, 1) and contain theSameElementsAs (right2))
+        left should (not contain inOrder (1, 2, 8) and contain theSameElementsAs (right2))
+        left should (not contain oneOf (6, 8, 5) and contain theSameElementsAs (right2))
+        left should (not contain only (8, 1, 2) and contain theSameElementsAs (right2))
+        left should (not contain inOrderOnly (1, 2, 8) and contain theSameElementsAs (right2))
+        left should (not contain noneOf (1, 2, 8) and contain theSameElementsAs (right2))
+        
+      }
+      
+      def `should pass when contain and not contain passes` {
+        
+        val left = List(1, 2, 3)
+        val right1 = List(8, 1, 2)
+        val right2 = List(1, 2, 3)
+        
+        left should (contain theSameElementsAs (right2) and not contain theSameElementsAs (right1)) 
+        left should (contain theSameElementsAs (right2) and not contain theSameIteratedElementsAs (right1))
+        left should (contain theSameElementsAs (right2) and not contain allOf (8, 2, 1))
+        left should (contain theSameElementsAs (right2) and not contain inOrder (1, 2, 8))
+        left should (contain theSameElementsAs (right2) and not contain oneOf (6, 8, 5))
+        left should (contain theSameElementsAs (right2) and not contain only (8, 1, 2))
+        left should (contain theSameElementsAs (right2) and not contain inOrderOnly (1, 2, 8))
+        left should (contain theSameElementsAs (right2) and not contain noneOf (1, 2, 8))
+      }
+      
+      def `should pass when not contain and not contain passes` {
+        
+        val left = List(1, 2, 3)
+        val right1 = List(8, 1, 2)
+        val right2 = List(1, 2, 8)
+        
+        left should (not contain theSameElementsAs (right2) and not contain theSameElementsAs (right1)) 
+        left should (not contain theSameElementsAs (right2) and not contain theSameIteratedElementsAs (right1))
+        left should (not contain theSameElementsAs (right2) and not contain allOf (8, 2, 1))
+        left should (not contain theSameElementsAs (right2) and not contain inOrder (1, 2, 8))
+        left should (not contain theSameElementsAs (right2) and not contain oneOf (6, 8, 5))
+        left should (not contain theSameElementsAs (right2) and not contain only (8, 1, 2))
+        left should (not contain theSameElementsAs (right2) and not contain inOrderOnly (1, 2, 8))
+        left should (not contain theSameElementsAs (right2) and not contain noneOf (1, 2, 8))
+      }
+      
+      def `should failed with correctly stack depth and message when first not contain failed but second contain passed` {
+        val e = intercept[exceptions.TestFailedException] {
+          List(1, 2, 3) should (not contain theSameIteratedElementsAs (List(1, 2, 3)) and contain theSameElementsAs List(3, 2, 1)) 
+        }
+        e.message should be (Some("List(1, 2, 3) contained the same iterated elements as List(1, 2, 3)"))
+        e.failedCodeFileName should be (Some("ContainMatcherAndOrSpec.scala"))
+        e.failedCodeLineNumber should be (Some(thisLineNumber - 4))
+      }
+      
+      def `should failed with correctly stack depth and message when first contain passed but second not contain failed` {
+        val e = intercept[exceptions.TestFailedException] {
+          List(1, 2, 3) should (contain theSameElementsAs List(3, 2, 1) and not contain theSameIteratedElementsAs (List(1, 2, 3))) 
+        }
+        e.message should be (Some("List(1, 2, 3) contained the same elements as List(3, 2, 1), but List(1, 2, 3) contained the same iterated elements as List(1, 2, 3)"))
+        e.failedCodeFileName should be (Some("ContainMatcherAndOrSpec.scala"))
+        e.failedCodeLineNumber should be (Some(thisLineNumber - 4))
+      }
+      
+      def `should failed with correctly stack depth and message when first not contain failed and second not contain failed` {
+        val e = intercept[exceptions.TestFailedException] {
+          List(1, 2, 3) should (not contain theSameIteratedElementsAs (List(1, 2, 3)) and not contain theSameElementsAs (List(3, 2, 1))) 
+        }
+        e.message should be (Some("List(1, 2, 3) contained the same iterated elements as List(1, 2, 3)"))
         e.failedCodeFileName should be (Some("ContainMatcherAndOrSpec.scala"))
         e.failedCodeLineNumber should be (Some(thisLineNumber - 4))
       }
@@ -67,6 +152,82 @@ class ContainMatcherAndOrSpec extends Spec with Matchers with SharedHelpers {
           List(1, 2, 3) should (contain theSameElementsAs List(3, 8, 1) or contain theSameIteratedElementsAs List(3, 2, 1)) 
         }
         e.message should be (Some("List(1, 2, 3) did not contain the same elements as List(3, 8, 1), and List(1, 2, 3) did not contain the same iterated elements as List(3, 2, 1)"))
+        e.failedCodeFileName should be (Some("ContainMatcherAndOrSpec.scala"))
+        e.failedCodeLineNumber should be (Some(thisLineNumber - 4))
+      }
+      
+      def `should pass when not contain and contain passes` {
+        
+        val left = List(1, 2, 3)
+        val right1 = List(8, 1, 2)
+        val right2 = List(1, 2, 3)
+        
+        left should (not contain theSameElementsAs (right1) or contain theSameElementsAs (right2)) 
+        left should (not contain theSameIteratedElementsAs (right1) or contain theSameElementsAs (right2))
+        left should (not contain allOf (8, 2, 1) or contain theSameElementsAs (right2))
+        left should (not contain inOrder (1, 2, 8) or contain theSameElementsAs (right2))
+        left should (not contain oneOf (6, 8, 5) or contain theSameElementsAs (right2))
+        left should (not contain only (8, 1, 2) or contain theSameElementsAs (right2))
+        left should (not contain inOrderOnly (1, 2, 8) or contain theSameElementsAs (right2))
+        left should (not contain noneOf (1, 2, 8) or contain theSameElementsAs (right2))
+        
+      }
+      
+      def `should pass when contain and not contain passes` {
+        
+        val left = List(1, 2, 3)
+        val right1 = List(8, 1, 2)
+        val right2 = List(1, 2, 3)
+        
+        left should (contain theSameElementsAs (right2) or not contain theSameElementsAs (right1)) 
+        left should (contain theSameElementsAs (right2) or not contain theSameIteratedElementsAs (right1))
+        left should (contain theSameElementsAs (right2) or not contain allOf (8, 2, 1))
+        left should (contain theSameElementsAs (right2) or not contain inOrder (1, 2, 8))
+        left should (contain theSameElementsAs (right2) or not contain oneOf (6, 8, 5))
+        left should (contain theSameElementsAs (right2) or not contain only (8, 1, 2))
+        left should (contain theSameElementsAs (right2) or not contain inOrderOnly (1, 2, 8))
+        left should (contain theSameElementsAs (right2) or not contain noneOf (1, 2, 8))
+      }
+      
+      def `should pass when not contain and not contain passes` {
+        
+        val left = List(1, 2, 3)
+        val right1 = List(8, 1, 2)
+        val right2 = List(1, 2, 8)
+        
+        left should (not contain theSameElementsAs (right2) or not contain theSameElementsAs (right1)) 
+        left should (not contain theSameElementsAs (right2) or not contain theSameIteratedElementsAs (right1))
+        left should (not contain theSameElementsAs (right2) or not contain allOf (8, 2, 1))
+        left should (not contain theSameElementsAs (right2) or not contain inOrder (1, 2, 8))
+        left should (not contain theSameElementsAs (right2) or not contain oneOf (6, 8, 5))
+        left should (not contain theSameElementsAs (right2) or not contain only (8, 1, 2))
+        left should (not contain theSameElementsAs (right2) or not contain inOrderOnly (1, 2, 8))
+        left should (not contain theSameElementsAs (right2) or not contain noneOf (1, 2, 8))
+      }
+      
+      def `should failed with correctly stack depth and message when first not contain failed and second contain failed` {
+        val e = intercept[exceptions.TestFailedException] {
+          List(1, 2, 3) should (not contain theSameIteratedElementsAs (List(1, 2, 3)) or contain theSameElementsAs List(8, 2, 1)) 
+        }
+        e.message should be (Some("List(1, 2, 3) contained the same iterated elements as List(1, 2, 3), and List(1, 2, 3) did not contain the same elements as List(8, 2, 1)"))
+        e.failedCodeFileName should be (Some("ContainMatcherAndOrSpec.scala"))
+        e.failedCodeLineNumber should be (Some(thisLineNumber - 4))
+      }
+      
+      def `should failed with correctly stack depth and message when first contain failed and second not contain failed` {
+        val e = intercept[exceptions.TestFailedException] {
+          List(1, 2, 3) should (contain theSameIteratedElementsAs (List(3, 2, 1)) or not contain theSameElementsAs (List(3, 2, 1))) 
+        }
+        e.message should be (Some("List(1, 2, 3) did not contain the same iterated elements as List(3, 2, 1), and List(1, 2, 3) contained the same elements as List(3, 2, 1)"))
+        e.failedCodeFileName should be (Some("ContainMatcherAndOrSpec.scala"))
+        e.failedCodeLineNumber should be (Some(thisLineNumber - 4))
+      }
+      
+      def `should failed with correctly stack depth and message when first not contain failed and second not contain failed` {
+        val e = intercept[exceptions.TestFailedException] {
+          List(1, 2, 3) should (not contain theSameIteratedElementsAs (List(1, 2, 3)) or not contain theSameElementsAs (List(3, 2, 1))) 
+        }
+        e.message should be (Some("List(1, 2, 3) contained the same iterated elements as List(1, 2, 3), and List(1, 2, 3) contained the same elements as List(3, 2, 1)"))
         e.failedCodeFileName should be (Some("ContainMatcherAndOrSpec.scala"))
         e.failedCodeLineNumber should be (Some(thisLineNumber - 4))
       }
