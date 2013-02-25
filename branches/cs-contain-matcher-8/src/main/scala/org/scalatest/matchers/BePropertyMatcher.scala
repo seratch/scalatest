5c94ca6d1e5abd4995a1082fb546ae66dccb8550
chuacheeseng83@gmail.com <chuacheeseng83@gmail.com@fd1274fd-080f-5963-8033-43ee40fcd1de>
2013-02-25 02:51:25 +0000

    Created cs-contain-matcher-8 branch from trunk (rev. 5015) to get latest Equality stuff in trunk for subsequence work in cs-contain-matcher-7 branch.

---
+++ branches/cs-contain-matcher-8/src/main/scala/org/scalatest/matchers/BePropertyMatcher.scala
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2001-2008 Artima, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.scalatest.matchers
+
+import org.scalatest._
+
+// TODO: Deprecate this one
+
+/**
+ * Companion object for trait <code>BePropertyMatcher</code> that provides a
+ * factory method that creates a <code>BePropertyMatcher[T]</code> from a
+ * passed function of type <code>(T => BePropertyMatchResult)</code>.
+ *
+ * @author Bill Venners
+ */
+object BePropertyMatcher {
+
+  /**
+   * Factory method that creates a <code>BePropertyMatcher[T]</code> from a
+   * passed function of type <code>(T => BePropertyMatchResult)</code>.
+   *
+   * @author Bill Venners
+   */
+  def apply[T](fun: T => BePropertyMatchResult): BePropertyMatcher[T] =
+    new BePropertyMatcher[T] {
+      def apply(left: T) = fun(left)
+    }
+}
