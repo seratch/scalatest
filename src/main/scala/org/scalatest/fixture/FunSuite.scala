718f459c5853d38177e1bf971c4c2ea7ca7a573a
george.berger <george.berger@fd1274fd-080f-5963-8033-43ee40fcd1de>
2013-02-26 20:37:32 +0000

    branches/to-release-as-1.9.2-for-scala-2.9.x: refactor of style traits into classes

--- src/main/scala/org/scalatest/fixture/FunSuite.scala
+++ src/main/scala/org/scalatest/fixture/FunSuite.scala
@@ -26,7 +26,7 @@ import FunSuite.IgnoreTagName
 import org.scalatest.Suite.checkRunTestParamsForNull
 
 /**
- * A sister trait to <code>org.scalatest.FunSuite</code> that can pass a fixture object into its tests.
+ * A sister class to <code>org.scalatest.FunSuite</code> that can pass a fixture object into its tests.
  *
  * <p>
  * The purpose of <code>fixture.FunSuite</code> and its subtraits is to facilitate writing tests in
@@ -39,20 +39,20 @@ import org.scalatest.Suite.checkRunTestParamsForNull
  * </p>
  *
  * <p>
- * Trait <code>fixture.FunSuite</code> behaves similarly to trait <code>org.scalatest.FunSuite</code>, except that tests may have a
+ * Class <code>fixture.FunSuite</code> behaves similarly to class <code>org.scalatest.FunSuite</code>, except that tests may have a
  * fixture parameter. The type of the
- * fixture parameter is defined by the abstract <code>FixtureParam</code> type, which is declared as a member of this trait.
- * This trait also declares an abstract <code>withFixture</code> method. This <code>withFixture</code> method
- * takes a <code>OneArgTest</code>, which is a nested trait defined as a member of this trait.
+ * fixture parameter is defined by the abstract <code>FixtureParam</code> type, which is a member of this class.
+ * This class also has an abstract <code>withFixture</code> method. This <code>withFixture</code> method
+ * takes a <code>OneArgTest</code>, which is a nested trait defined as a member of this class.
  * <code>OneArgTest</code> has an <code>apply</code> method that takes a <code>FixtureParam</code>.
  * This <code>apply</code> method is responsible for running a test.
- * This trait's <code>runTest</code> method delegates the actual running of each test to <code>withFixture</code>, passing
- * in the test code to run via the <code>OneArgTest</code> argument. The <code>withFixture</code> method (abstract in this trait) is responsible
+ * This class's <code>runTest</code> method delegates the actual running of each test to <code>withFixture</code>, passing
+ * in the test code to run via the <code>OneArgTest</code> argument. The <code>withFixture</code> method (abstract in this class) is responsible
  * for creating the fixture argument and passing it to the test function.
  * </p>
  * 
  * <p>
- * Subclasses of this trait must, therefore, do three things differently from a plain old <code>org.scalatest.FunSuite</code>:
+ * Subclasses of this class must, therefore, do three things differently from a plain old <code>org.scalatest.FunSuite</code>:
  * </p>
  * 
  * <ol>
@@ -136,7 +136,7 @@ import org.scalatest.Suite.checkRunTestParamsForNull
  *
  * <p>
  * If the fixture you want to pass into your tests consists of multiple objects, you will need to combine
- * them into one object to use this trait. One good approach to passing multiple fixture objects is
+ * them into one object to use this class. One good approach to passing multiple fixture objects is
  * to encapsulate them in a case class. Here's an example:
  * </p>
  *
@@ -182,7 +182,7 @@ import org.scalatest.Suite.checkRunTestParamsForNull
  * externally so that the file name can be can be changed from run to run. To accomplish this
  * the <code>OneArgTest</code> trait has a <code>configMap</code>
  * method, which will return a <code>Map[String, Any]</code> from which configuration information may be obtained.
- * The <code>runTest</code> method of this trait will pass a <code>OneArgTest</code> to <code>withFixture</code>
+ * The <code>runTest</code> method of this class will pass a <code>OneArgTest</code> to <code>withFixture</code>
  * whose <code>configMap</code> method returns the <code>configMap</code> passed to <code>runTest</code>.
  * Here's an example in which the name of a temp file is taken from the passed <code>configMap</code>:
  * </p>
@@ -366,167 +366,4 @@ import org.scalatest.Suite.checkRunTestParamsForNull
  *
  * @author Bill Venners
  */
-trait FunSuite extends Suite { thisSuite =>
-
-  private final val engine = new FixtureEngine[FixtureParam]("concurrentFixtureFunSuiteMod", "FixtureFunSuite")
-  import engine._
-  
-  private[scalatest] val sourceFileName = "FunSuite.scala"
-
-  /**
-   * Returns an <code>Informer</code> that during test execution will forward strings (and other objects) passed to its
-   * <code>apply</code> method to the current reporter. If invoked in a constructor, it
-   * will register the passed string for forwarding later during test execution. If invoked while this
-   * <code>fixture.FunSuite</code> is being executed, such as from inside a test function, it will forward the information to
-   * the current reporter immediately. If invoked at any other time, it will
-   * throw an exception. This method can be called safely by any thread.
-   */
-  implicit protected def info: Informer = atomicInformer.get
-
-  /**
-   * Register a test with the specified name, optional tags, and function value that takes no arguments.
-   * This method will register the test for later execution via an invocation of one of the <code>run</code>
-   * methods. The passed test name must not have been registered previously on
-   * this <code>FunSuite</code> instance.
-   *
-   * @param testName the name of the test
-   * @param testTags the optional list of tags for this test
-   * @param testFun the test function
-   * @throws TestRegistrationClosedException if invoked after <code>run</code> has been invoked on this suite
-   * @throws DuplicateTestNameException if a test with the same name has been registered previously
-   * @throws NotAllowedException if <code>testName</code> had been registered previously
-   * @throws NullPointerException if <code>testName</code> or any passed test tag is <code>null</code>
-   */
-  protected def test(testName: String, testTags: Tag*)(testFun: FixtureParam => Any) {
-    registerTest(testName, testFun, "testCannotAppearInsideAnotherTest", sourceFileName, "test", 2, None, None, testTags: _*)
-  }
-
-  /**
-   * Register a test to ignore, which has the specified name, optional tags, and function value that takes no arguments.
-   * This method will register the test for later ignoring via an invocation of one of the <code>run</code>
-   * methods. This method exists to make it easy to ignore an existing test by changing the call to <code>test</code>
-   * to <code>ignore</code> without deleting or commenting out the actual test code. The test will not be run, but a
-   * report will be sent that indicates the test was ignored. The passed test name must not have been registered previously on
-   * this <code>FunSuite</code> instance.
-   *
-   * @param testName the name of the test
-   * @param testTags the optional list of tags for this test
-   * @param testFun the test function
-   * @throws TestRegistrationClosedException if invoked after <code>run</code> has been invoked on this suite
-   * @throws DuplicateTestNameException if a test with the same name has been registered previously
-   * @throws NotAllowedException if <code>testName</code> had been registered previously
-   */
-  protected def ignore(testName: String, testTags: Tag*)(testFun: FixtureParam => Any) {
-    registerIgnoredTest(testName, testFun, "ignoreCannotAppearInsideATest", sourceFileName, "ignore", 1, testTags: _*)
-  }
-
-  /**
-  * An immutable <code>Set</code> of test names. If this <code>fixture.FunSuite</code> contains no tests, this method returns an empty <code>Set</code>.
-  *
-  * <p>
-  * This trait's implementation of this method will return a set that contains the names of all registered tests. The set's iterator will
-  * return those names in the order in which the tests were registered.
-  * </p>
-  */
-  override def testNames: Set[String] = {
-    // I'm returning a ListSet here so that they tests will be run in registration order
-    ListSet(atomic.get.testNamesList.toArray: _*)
-  }
-
-  /**
-   * Run a test. This trait's implementation runs the test registered with the name specified by <code>testName</code>.
-   *
-   * @param testName the name of one test to run.
-   * @param reporter the <code>Reporter</code> to which results will be reported
-   * @param stopper the <code>Stopper</code> that will be consulted to determine whether to stop execution early.
-   * @param configMap a <code>Map</code> of properties that can be used by the executing <code>Suite</code> of tests.
-   * @throws IllegalArgumentException if <code>testName</code> is defined but a test with that name does not exist on this <code>fixture.FunSuite</code>
-   * @throws NullPointerException if any of <code>testName</code>, <code>reporter</code>, <code>stopper</code>, or <code>configMap</code>
-   *     is <code>null</code>.
-   */
-  protected override def runTest(testName: String, reporter: Reporter, stopper: Stopper, configMap: Map[String, Any], tracker: Tracker) {
-
-    def invokeWithFixture(theTest: TestLeaf) {
-      theTest.testFun match {
-        case wrapper: NoArgTestWrapper[_] =>
-          withFixture(new FixturelessTestFunAndConfigMap(testName, wrapper.test, configMap))
-        case fun => withFixture(new TestFunAndConfigMap(testName, fun, configMap))
-      }
-    }
-
-    runTestImpl(thisSuite, testName, reporter, stopper, configMap, tracker, true, invokeWithFixture)
-  }
-
-  /**
-   * A <code>Map</code> whose keys are <code>String</code> tag names to which tests in this <code>fixture.FunSuite</code> belong, and values
-   * the <code>Set</code> of test names that belong to each tag. If this <code>fixture.FunSuite</code> contains no tags, this method returns an empty
-   * <code>Map</code>.
-   *
-   * <p>
-   * This trait's implementation returns tags that were passed as strings contained in <code>Tag</code> objects passed to
-   * methods <code>test</code> and <code>ignore</code>.
-   * </p>
-   */
-  override def tags: Map[String, Set[String]] = atomic.get.tagsMap
-
-  protected override def runTests(testName: Option[String], reporter: Reporter, stopper: Stopper, filter: Filter,
-      configMap: Map[String, Any], distributor: Option[Distributor], tracker: Tracker) {
-
-    runTestsImpl(thisSuite, testName, reporter, stopper, filter, configMap, distributor, tracker, info, true, runTest)
-  }
-
-  override def run(testName: Option[String], reporter: Reporter, stopper: Stopper, filter: Filter,
-      configMap: Map[String, Any], distributor: Option[Distributor], tracker: Tracker) {
-
-    runImpl(thisSuite, testName, reporter, stopper, filter, configMap, distributor, tracker, super.run)
-  }
-
-  /**
-   * Registers shared tests.
-   *
-   * <p>
-   * This method enables the following syntax for shared tests in a <code>fixture.FunSuite</code>:
-   * </p>
-   *
-   * <pre class="stHighlight">
-   * testsFor(nonEmptyStack(lastValuePushed))
-   * </pre>
-   *
-   * <p>
-   * This method just provides syntax sugar intended to make the intent of the code clearer.
-   * Because the parameter passed to it is
-   * type <code>Unit</code>, the expression will be evaluated before being passed, which
-   * is sufficient to register the shared tests. For examples of shared tests, see the
-   * <a href="../FunSuite.html#SharedTests">Shared tests section</a> in the main documentation for
-   * trait <code>FunSuite</code>.
-   * </p>
-   */
-  protected def testsFor(unit: Unit) {}
-
-  /**
-   * Implicitly converts a function that takes no parameters and results in <code>PendingNothing</code> to
-   * a function from <code>FixtureParam</code> to <code>Any</code>, to enable pending tests to registered as by-name parameters
-   * by methods that require a test function that takes a <code>FixtureParam</code>.
-   *
-   * <p>
-   * This method makes it possible to write pending tests as simply <code>(pending)</code>, without needing
-   * to write <code>(fixture => pending)</code>.
-   * </p>
-   */
-  protected implicit def convertPendingToFixtureFunction(f: => PendingNothing): (FixtureParam => Any) = {
-    fixture => f
-  }
-
-  /**
-   * Implicitly converts a function that takes no parameters and results in <code>Any</code> to
-   * a function from <code>FixtureParam</code> to <code>Any</code>, to enable no-arg tests to registered
-   * by methods that require a test function that takes a <code>FixtureParam</code>.
-   */
-  protected implicit def convertNoArgToFixtureFunction(fun: () => Any): (FixtureParam => Any) =
-    new NoArgTestWrapper(fun)
-  
-  /**
-   * Suite style name.
-   */
-  final override val styleName: String = "org.scalatest.fixture.FunSuite"
-}
+abstract class FunSuite extends FunSuiteLike
