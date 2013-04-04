51dab2cf0e81079da959f5d59b965253fc5d3ec9
chuacheeseng83@gmail.com <chuacheeseng83@gmail.com@fd1274fd-080f-5963-8033-43ee40fcd1de>
2013-04-04 08:15:35 +0000

    cs-sbt-5 branch - Removed DoNotDiscoverFingerprint in Framework, and changed its Runner.task (without selector) to return Task to does nothing when the Suite is annotated with @DoNotDiscover.

--- branches/cs-sbt-5/src/test/scala/org/scalatest/tools/scalasbt/SuiteWithNestedSuites.scala
+++ branches/cs-sbt-5/src/test/scala/org/scalatest/tools/scalasbt/SuiteWithNestedSuites.scala
@@ -1,12 +1,21 @@
 package org.scalatest.tools.scalasbt
 
-import org.scalatest.FunSuite
+import org.scalatest._
 
 class SuiteWithNestedSuites extends FunSuite {
   
   test("test 1") {}
   test("test 2") {}
   test("test 3") {}
+  
+  @DoNotDiscover
+  class NestedDoNotDiscoverSuite extends FunSuite {
+    override def suiteId = "nested 2"
+          
+    test("nested 2 test 1") {}
+    test("nested 2 test 2") {}
+    test("nested 2 test 3") {}
+  }
   
   override def nestedSuites = 
     collection.immutable.IndexedSeq(
@@ -17,12 +26,6 @@ class SuiteWithNestedSuites extends FunSuite {
         test("nested 1 test 2") {}
         test("nested 1 test 3") {}
       }, 
-      new FunSuite() {
-        override def suiteId = "nested 2"
-          
-        test("nested 2 test 1") {}
-        test("nested 2 test 2") {}
-        test("nested 2 test 3") {}
-      }
+      new NestedDoNotDiscoverSuite
     )
 }
